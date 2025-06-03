94114f46ca3daa03ffa715f31e059f218ffdc3a9
eNV25 <env252525@gmail.com>
2025-06-03 19:23:03 +0200

    build.sh: Add public key to apk trusted keys.

--- build.sh
+++ build.sh
@@ -4,18 +4,10 @@ doas sed -i 's/edge/v3.21/g' /etc/apk/repositories
 doas apk upgrade -U --available
 doas chown -R "$(id -u)" "$GITHUB_WORKSPACE"
 
-[ -n "$RSA_PRIVATE_KEY" ] || {
-  echo "RSA_PRIVATE_KEY is empty"
-  exit 1
-}
-[ -n "$RSA_PUBLIC_KEY" ] || {
-  echo "RSA_PUBLIC_KEY is empty"
-  exit 1
-}
-
 echo "$RSA_PRIVATE_KEY" >"$GITHUB_WORKSPACE/$GITHUB_REPOSITORY_OWNER.rsa"
 echo "$RSA_PUBLIC_KEY" >"$GITHUB_WORKSPACE/$GITHUB_REPOSITORY_OWNER.rsa.pub"
 chmod 600 "$GITHUB_WORKSPACE/$GITHUB_REPOSITORY_OWNER.rsa"
+doas cp "$GITHUB_WORKSPACE/$GITHUB_REPOSITORY_OWNER.rsa.pub" /etc/apk/keys/
 
 export REPODEST="$GITHUB_WORKSPACE"
 export PACKAGER="eNV25 <env252525@gmail.com>"
